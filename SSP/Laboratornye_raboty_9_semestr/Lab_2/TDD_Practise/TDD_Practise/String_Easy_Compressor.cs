f1ac5bb16df42fc49213730e07a0b5782f252b6c
Aliaksandr Labanau1 <Aliaksandr_Labanau1@epam.com>
2018-02-20 14:01:48 +0300

    Lab_2(TDD):finished. Created unit test for string compressor class and implemented functionality for string comprssor class

---
+++ SSP/Laboratornye_raboty_9_semestr/Lab_2/TDD_Practise/TDD_Practise/String_Easy_Compressor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TDD_Practise
+{
+    public struct CharChainMatch
+    {
+        public int startPosition;
+        public int length;
+        public char letter;
+
+        public CharChainMatch(int startPosition, int length,char letter)
+        {
+            this.startPosition = startPosition;
+            this.length = length;
+            this.letter = letter;
+        }
+    }
+
+    public class String_Easy_Compressor
+    {
+        public String_Easy_Compressor()
+        {
+        }
+
+        public string Compress(string text)
+        {
+            int currentPosition = 0;
+            CharChainMatch? currentMatch = null;
+            while(currentPosition < text.Length)
+            {
+                currentMatch = matchSampleCharactersChain(text, currentPosition);
+                if(currentMatch.HasValue)
+                {
+                    text = text.Remove(currentMatch.Value.startPosition, currentMatch.Value.length).Insert(currentMatch.Value.startPosition, currentMatch.Value.letter + currentMatch.Value.length.ToString());
+                    currentPosition = currentMatch.Value.startPosition + 2;
+                } else
+                {
+                    break;
+                }
+            }
+            return text;
+        }
+
+        public CharChainMatch? matchSampleCharactersChain(string text, int startPosition, int minChainLength = 2)
+        {
+            CharChainMatch? chainMatch = null;
+            int currentPosition = startPosition, startMatchPosition = currentPosition, numberOfMatches = 0;
+            char currentLetter = text[currentPosition];
+
+            if (currentPosition >= text.Length)
+            {
+                return null;
+            }
+
+            for(; currentPosition < text.Length; currentPosition++)
+            {
+                if(Char.IsLetter(text[currentPosition]) && currentLetter == text[currentPosition])
+                {
+                    numberOfMatches++;
+
+                } else if(numberOfMatches >= minChainLength)
+                {
+                    chainMatch = new CharChainMatch(startMatchPosition, numberOfMatches, currentLetter);
+                    break;
+
+                } else
+                {
+                    numberOfMatches = 1;
+                    startMatchPosition = currentPosition;
+                    currentLetter = text[currentPosition];
+                }
+            }
+            return chainMatch;
+        }
+    }
+}
---
+++ SSP/Laboratornye_raboty_9_semestr/Lab_2/TDD_Practise/TTD_Practise.Tests/TDD_Practise_Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_Practise;
+
+namespace TTD_Practise.Tests
+{
+    [TestClass]
+    public class TDD_Practise_Test
+    {
+        [TestMethod]
+        public void ShouldCompressOnlyLetters()
+        {
+            String_Easy_Compressor compressor = new String_Easy_Compressor();
+
+            string compresedString = compressor.Compress("((aaabb111 ccccddd)(eeefggg hhkl))");
+            string testSample = "((a3b2111 c4d3)(e3fg3 h2kl))";
+
+            Assert.AreEqual<string>(compresedString, testSample);
+        }
+    }
+}
